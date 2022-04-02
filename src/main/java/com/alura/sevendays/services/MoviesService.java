d508abe0318960a8da81ce9d71a09320d82fa9a9
Renato Martins <quessuadev@gmail.com>
2022-04-02 20:25:14 +0100

    Day 2 :white_check_mark:

--- src/main/java/com/alura/sevendays/services/MoviesService.java
+++ src/main/java/com/alura/sevendays/services/MoviesService.java
@@ -1,6 +1,9 @@
 package com.alura.sevendays.services;
 
 import com.alura.sevendays.SevendaysApplication;
+import com.alura.sevendays.models.Top250Data;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,7 +17,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Component
@@ -28,15 +33,74 @@ public class MoviesService {
     @Value("${imdb.api.key}")
     public String imdb_api_key;
 
+
+
+    /***
+     * Get top 250 movies but arranged as an object.
+     * @return
+     */
+    public Top250Data GetTop250MoviesToObject(){
+
+        String api_url = imdb_url + "/" + imdb_api_key;
+
+        HttpResponse webCallResponse=MakeACall(api_url);
+
+        ObjectMapper mapper=new ObjectMapper();
+        Top250Data top250Data= null;
+        try {
+            top250Data = mapper.readValue(webCallResponse.body().toString(), Top250Data.class);
+
+            if(top250Data!=null){
+
+                logger.info("List of Titles");
+                top250Data.getItems().stream().filter(p->p.getTitle()!=null).forEach(System.out::println);
+                logger.info("List of Images urls");
+                top250Data.getItems().stream().filter(p->p.getImage()!=null).forEach(System.out::println);
+            }
+
+        } catch (JsonProcessingException | NullPointerException e) {
+            logger.error(e.getMessage());
+            e.printStackTrace();
+        }
+
+        return top250Data;
+
+    }
+    private HttpResponse MakeACall(String url) {
+
+        HttpClient client = HttpClient.newBuilder().build();
+
+        HttpRequest request = null;
+        try {
+            request = HttpRequest.newBuilder()
+                    .uri(new URI(url))
+                    .GET()
+                    .build();
+
+            HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response;
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            logger.error(e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+    /**
+     * GetTop250Movies
+     * Service used to get a list of top 250 movies from IMDB
+     *
+     * @return
+     */
     public Map<Integer, Object> GetTop250Movies() {
 
         String api_url = imdb_url + "/" + imdb_api_key;
 
-
-
         HttpClient client = HttpClient.newBuilder().build();
         try {
-            var request =  HttpRequest.newBuilder()
+            var request = HttpRequest.newBuilder()
                     .uri(new URI(api_url))
                     .GET()
                     .build();
--- src/test/java/com/alura/sevendays/services/MovieServicesTest.java
+++ src/test/java/com/alura/sevendays/services/MovieServicesTest.java
@@ -1,5 +1,6 @@
 package com.alura.sevendays.services;
 
+import com.alura.sevendays.models.Top250Data;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +21,16 @@ public class MovieServicesTest {
         Map<Integer, Object> map=service.GetTop250Movies();
 
         assertThat(map.containsKey(200)).isTrue();
+
+    }
+
+    @Test
+    public void TestGetTop250MoviesToObject(){
+
+        Top250Data top250Data=service.GetTop250MoviesToObject();
+
+        assertThat(top250Data).isNotNull();
+        assertThat(top250Data.ErrorMessage).isEmpty();
 
     }
 }
